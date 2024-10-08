6d2a4eb7aef25d63b2cc5e2737acf9842c48db43
guapstarlik <143760831+guapstarlik@users.noreply.github.com>
2024-10-08 12:07:40 -0400

    fixed image layout

--- 4.1_Brat_Box/script.js
+++ 4.1_Brat_Box/script.js
@@ -2,7 +2,7 @@ const bratbox = document.getElementById('bratbox');
 let clickCount = 0;
 let backgroundType = 'color';
 
-const colors = ['#2208fd', '#f0ff00', '#e27294', '#846a32', '#a3d1bf'];
+const colors = ['#8ACE00','#2208fd', '#f0ff00', '#e27294', '#846a32', '#a3d1bf'];
 const images = ['Assets/bobby1.png', 'Assets/pic2.png', 'Assets/pic3.png', 'Assets/pic4.png'];
 const videos = ['Assets/video.MP4', 'Assets/video3.mp4'];
 
