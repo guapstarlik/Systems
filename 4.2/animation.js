f26a1b3595a754aab64875590aa52b86145a3ecc
guapstarlik <malik1017@hotmail.com>
2024-10-19 15:50:42 -0400

    5.2

--- 4.2/animation.js
+++ 4.2/animation.js
@@ -1,27 +1,41 @@
+   document.addEventListener("DOMContentLoaded", function() {
 
-
-    document.addEventListener("DOMContentLoaded", function() {
-    
         Draggable.create(".Food img", {
             type: "x,y",
             edgeResistance: 0.65,
             bounds: "body",
             inertia: true,
             onDrag: function() {
-               var men = document.querySelectorAll('.Men');
-            for (var i = 0; i < men.length; i++) {
-                if (this.hitTest(men[i])) {
-            
-                    if ((this.target.id === "burger" && men[i].id === "burgerman") ||
-                        (this.target.id === "hotdog" && men[i].id === "hotdogman") ||
-                        (this.target.classList.contains("Food") && men[i].id === "foodman")) {
-                        TweenLite.to(this.target, 1, {opacity: 0, scale: 0});
-                        break;
-                    }
-                }
+                if(this.hitTest(".Men img")) {
+    TweenLite.to(this.target, 1, {opacity:0, scale:0});
+                };
             }
-        }
-    });
+        });
+
+        Draggable.create("#hotdog ", {
+            type: "x,y",
+            edgeResistance: 0.65,
+            bounds: "body",
+            inertia: true,
+            onDrag: function() {
+                if(this.hitTest("#hotdogman ")) {
+    TweenLite.to(this.target, 1, {opacity:0, scale:0});
+                };
+            }
+        });
+
+        Draggable.create("#burger ", {
+            type: "x,y",
+            edgeResistance: 0.65,
+            bounds: "body",
+            inertia: true,
+            onDrag: function() {
+                if(this.hitTest("#burgerman ")) {
+    TweenLite.to(this.target, 1, {opacity:0, scale:0});
+                };
+            }
+        });
+
 
         
 
